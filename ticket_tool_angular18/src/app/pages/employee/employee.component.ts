3a6c1ea424feac556e6a78f800caec5dd3f0eaa8
Amit singh Rawat <amitrawat1804@gmail.com>
2024-09-15 21:40:48 +0530

    commit changes

--- ticket_tool_angular18/src/app/pages/employee/employee.component.ts
+++ ticket_tool_angular18/src/app/pages/employee/employee.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../services/master.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
@@ -12,24 +13,25 @@ export class EmployeeComponent implements OnInit{
 masterSrc=inject(MasterService);
 departList:any []=[];
 roleList: any []=[];
-
-
+empList:any[]=[];
+isNewView:boolean=false;
 
 newEmployeeObj :any={
   
   employeeId: 0,
-  employeeName: "string",
-  contactNo: "string",
-  emailId: "string",
+  employeeName: "",
+  contactNo: "",
+  emailId: "",
   deptId: 0,
-  password: "string",
-  gender: "string",
-  role: "string"
+  password: "",
+  gender: "",
+  role: ""
 }
 
 ngOnInit(): void {
   this.getAllDepart();
   this.getAllRole();
+  this.getAllEmployee();
 }
 
 getAllDepart(){
@@ -45,5 +47,56 @@ this.roleList=res.data;
   });
 }
 
+getAllEmployee(){
+  this.masterSrc.getAllEmployee().subscribe((res:any)=>{
+    this.empList=res.data;
+  })
+}
+
+
+saveEmp(){
+  this.masterSrc.CreateEmployee(this.newEmployeeObj).subscribe((res:any)=>{
+if(res.result){
+  alert("New Employee Created");
+  this.getAllEmployee();
+}
+else{
+  alert(res.message);
+}
+  });
+}
+
+onEdit(data: any){
+  this.newEmployeeObj=data;
+}
+
+
+updateEmployee(){
+  this.masterSrc.updateEmployee(this.newEmployeeObj).subscribe((res:any)=>{
+    if(res.result){
+      alert("New Employee Created");
+      this.getAllEmployee();
+    }
+    else{
+      alert(res.message);
+    }
+  });
+}
+
+deleteEmployee(id:number){
+  const isDeleted=confirm("Are you want to delete this");
+  if(isDeleted){
+    this.masterSrc.deleteEmployee(id).subscribe((res:any)=>{
+      if(res.result){
+        alert("deleted employee");
+        this.getAllEmployee();
+      }
+      else{
+        alert(res.message);
+      }
+    });
+  }
+  
+}
 
 }
